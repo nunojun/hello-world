e61326bea1125eb422073538a51f1ddbbedf1056
Bumjoon Kim <nunojun@gmail.com>
2016-06-27 14:44:31 -0600

    Added another line on HelloWorld.java to check gitconfig.

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Another line for a pull request");
+        System.out.println("Another line for checking gitconfig..");
     }
 }
